01adfe6fb00357745070938b67233a9a09e7107a
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-03-09 09:32:34 +0000

    JS loops

--- findNemo.js
+++ findNemo.js
@@ -1,6 +1,7 @@
 
+//Big O & Different types of loops in JS
 const nemo = ['nemo'];
-const everyone = ['dory','bruce', 'marlin', 'gill', 'john', 'nemo', 'boat', 'nigel', 'marla', 'squirt', 'nemo','hank'];
+const everyone = ['dory','bruce', 'marlin', 'gill', 'john', 'nemo', 'boat', 'nigel', 'marla', 'squirt','hank'];
 const large = new Array(100000).fill('nemo');
 
 function findNemo(array){
@@ -11,8 +12,27 @@ for(i=0; i<array.length; i++){
     break;
   }
 }
+}
+findNemo(everyone);  // Big O notation: O(n) --> Linear time.
 
-
+//forEach loop
+const findNemo2 = array => {
+  array.forEach(fish => {
+    if (fish === 'nemo'){
+      console.log('Found Nemo2');
+    }
+  })
 }
 
-findNemo(everyone);  // Big O notation: O(n) --> Linear time.
+findNemo2(everyone);
+
+//for of loop
+const findNemo3 = array => {
+ for(let fish of array){
+    if (fish === 'nemo'){
+      console.log('Found Nemo3');
+    }
+  }
+}
+
+findNemo3(everyone);
