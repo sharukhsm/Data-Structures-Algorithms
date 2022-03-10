fed2b9042392f6d818aa483293c42c9511c145b5
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-03-10 13:23:37 +0000

    add comments

--- googleQuestion.js
+++ googleQuestion.js
@@ -1,7 +1,7 @@
 // [1,2,3,9] sum = 8
 // [1,2,4,4] sum = 8
 
-//Naive
+//Naive 
 function hasPairWithSum(arr, sum){
   for(i=0; i<arr.length-1; i++){
     for(j=0; j<arr.length-1; j++){
@@ -13,5 +13,7 @@ function hasPairWithSum(arr, sum){
   }
   return false;
 }
+
+//O(n^2) - Time complexity.
 
 console.log(hasPairWithSum([6,2,3,4],9));
