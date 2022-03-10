f3858584a9c32555ab974cb65b3be9ca79eda5b6
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-03-10 11:24:37 +0000

    complete interviewQuestion

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -1,4 +1,4 @@
-// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+// ---Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items.
 //For Example:
 //const array1 = ['a','b','c','x'];
 //const array2 = ['z','y','i'];
@@ -6,8 +6,9 @@
 //-----------
 //const array1 = ['a','b','c','x'];
 //const array2 = ['z','y','x'];
-//should return true.
+//should return true.    -----//
 
+// Inputs/outputs
 //2 parameters - arrays - No size limit
 //output - return True/false
 
@@ -26,17 +27,18 @@
 //  return false;
 // }
 
-// //O(a*b)
+// //O(a*b) - Time complexity
+// //O(1).  - Space complexity
 // console.log(compareArray(firstArray, secondArray));
 
 
 
-// //2. Optimizing the brute force solution. O(a+b)
+// //2. Optimizing the brute force solution to O(a+b).
 // const firstArray = ['a', 'b', 'c', 'x'];
 // const secondArray = ['d','e','a'];
 // function compareArray2(arr1, arr2){
 //   //loop through first array and create object where properties === items in the array.
-//   //loop through second array and check if item in second array exists on created object.
+
 // let map = {};
 //   for(let i=0; i< arr1.length; i++){
 //     if(!map[arr1[i]]){   //map[i] is false, ! to make it true.
@@ -47,6 +49,7 @@
 //   }
 // console.log(map);
 
+//loop through second array and check if item in second array exists on created owasd
 //   for(let j=0; j<arr2.length; j++){
 //    if(map[arr2[j]]){
 //      return true;
@@ -55,8 +58,8 @@
 //  return false;
 // }
 
-// //O(a+b) cuz two seperate for loops.
-
+// //O(a+b) - Time complexity, cuz two seperate for loops.
+// //O(a) - Space complexity.
 // console.log(compareArray2(firstArray, secondArray));
 
 
