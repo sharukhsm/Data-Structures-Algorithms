992cce908a5cfa14968515f04c2ca403f004bd60
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-03-10 13:14:05 +0000

    googleInterview basic solution

---
+++ googleQuestion.js
@@ -0,0 +1,17 @@
+// [1,2,3,9] sum = 8
+// [1,2,4,4] sum = 8
+
+//Naive
+function hasPairWithSum(arr, sum){
+  for(i=0; i<arr.length-1; i++){
+    for(j=0; j<arr.length-1; j++){
+      console.log(arr[i], arr[j]);
+      if(arr[i] + arr[j] === sum){
+     return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(hasPairWithSum([6,2,3,4],9));
