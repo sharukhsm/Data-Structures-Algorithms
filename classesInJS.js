271ec65fccd4e367b696be6349d0b42f8e5395a2
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-04-25 12:04:53 +0000

    Complete Instantiation in classes in JS

--- classesInJS.js
+++ classesInJS.js
@@ -16,3 +16,26 @@ const object4 = {
     console.log(this);
   }
 }
+
+// instantiation
+class Player{
+  constructor(name, type){
+    this.name = name;
+    this.type = type;
+  }
+  introduce(){
+    console.log(`Hi I'm ${this.name}, I'm a ${this.type}`)
+  }
+}
+
+class Wizard extends Player{
+  constructor(name, type){
+    super(name,type)
+    console.log('wizard', this);
+  }
+  play(){
+    console.log(`WEEEE I'm a ${this.type}`);
+  }
+}
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic');
