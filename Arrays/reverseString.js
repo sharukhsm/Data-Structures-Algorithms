f67943e77a9656f75fa48ad2951612ebf461b593
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-04-26 09:14:43 +0000

    Reverse array my first solution

---
+++ Arrays/reverseString.js
@@ -0,0 +1,27 @@
+// reverse the given string
+//Hi My name is Sharukh
+//hkurahS si eman yM iH
+
+//My solution
+// function reverse(str){
+// const str2 = str.split('')
+//   const totalItems = str2.length
+//   for(i=totalItems; i>=0; i--){
+//     console.log(str2[i])
+//   }
+ 
+// }
+
+// reverse('Hi My name is Sharukh')
+
+//Andrei solution 
+
+function reverse(str){
+  //check input
+  if (!str || str.length < 2 || typeof str !== 'string'){
+    return 'hmm that is not good'
+  }
+    
+}
+
+reverse('Hi My name is Sharukh')
