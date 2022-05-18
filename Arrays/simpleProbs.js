e4639e39b0d81a3d297951c603ea28a1b756cfbe
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2022-05-18 06:06:55 +0000

    add problems

---
+++ Arrays/simpleProbs.js
@@ -0,0 +1,42 @@
+//1. Add all items in an array
+    // var arr = [4, 8, 7, 13, 12]
+    // var sum = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr[i];
+    // }
+// console.log(sum);
+
+//Find if an item is present in an array.
+// var arr = [2, 6, 5, 7, 8]
+// num = 5
+// ans = 2
+//if the number isn't in the array return -1.
+
+// function isNumberPresent(num){
+//   var arr = [2, 6, 5, 7, 8];
+
+// for(let i=0; i<arr.length; i++){
+//   if(arr[i] === num){
+//     return i;
+//   } 
+// }
+//   return -1;
+// }
+// console.log(isNumberPresent(5));
+
+//fizzbuzz
+// if div by 3 print fizz, div by 5 buzz, if by both fizzbuzz, else print none
+// function fizzBuzz(num){
+//   if(num%3==0 && num%5==0 ){
+//     console.log('fizzBuzz');
+//   } else if(num%5==0){
+//     console.log('buzz')
+//   } else if (num%3==0){
+//     console.log('fizz')
+//   } else {
+//     console.log('none')
+//   }
+    
+// }
+
+// fizzBuzz(3);
